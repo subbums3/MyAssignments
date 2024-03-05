cf60e4555d20dcfefe7c466d638c9195f8adeab8
subbums3 <162311924+subbums3@users.noreply.github.com>
2024-03-05 16:38:29 +0530

    Add files via upload

---
+++ day1/Firefox.java
@@ -0,0 +1,15 @@
+package week1.day1;
+
+public class Firefox {
+	public static void main(String[] args) {
+		float browserVersion=100.2f;
+		String browserName="firefox";
+		boolean isVisible=true;
+		int releaseYear=1998;
+		char browserLogo='f';
+		System.out.println("browser version is="+browserVersion +" " +"browserName is="+browserName +" " +"isVisible =" +isVisible +" " +"releaseYear =" +releaseYear +" "  +"browserLogo = " +browserLogo);
+				
+		
+	}
+
+}
